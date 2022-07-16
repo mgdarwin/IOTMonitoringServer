10a80412233c2430f8756d98b557518dce7ebb32
Darwin_Moreno <d.moreno11@uniandes.edu.co>
2022-07-16 18:17:49 -0500

    se han configurado las IPs de los servers de viewer, database, MQQT broker v2

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "52.90.91.126"]
+ALLOWED_HOSTS = ["localhost", "54.175.13.79"]
 
 
 # Application definition
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "44.202.10.13",  # Dirección IP de la base de datos
+        "HOST": "54.172.115.157",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "18.208.109.119"
+MQTT_HOST = "54.159.185.61"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
